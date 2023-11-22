b4de614942fe2fad4b0c9722a073ff7d7aa0c527
juliemnt <julie.monnot@efrei.net>
2023-11-22 11:48:29 +0100

    Add Hello World ! in application server

---
+++ src/main/java/fr/efrei/backend/web/rest/ItemRessource.java
@@ -0,0 +1,16 @@
+package fr.efrei.backend.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ItemRessource {
+
+    @GetMapping ("/items")
+    public String test () {
+
+        return "Hello World !";
+    }
+}
