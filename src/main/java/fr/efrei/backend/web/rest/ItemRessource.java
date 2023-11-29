d4acb8f2b2fb91a5a4c2f9a3fb20946d979f3768
juliemnt <julie.monnot@efrei.net>
2023-11-29 14:17:15 +0100

    tests added

--- src/main/java/fr/efrei/backend/web/rest/ItemRessource.java
+++ src/main/java/fr/efrei/backend/web/rest/ItemRessource.java
@@ -1,16 +1,29 @@
 package fr.efrei.backend.web.rest;
 
+import fr.efrei.backend.domain.Item;
+import fr.efrei.backend.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ItemRessource {
 
-    @GetMapping ("/items")
-    public String test () {
+    public final ItemService itemService;
 
-        return "Hello World !";
+    public ItemRessource(ItemService itemService) {this.itemService = itemService; }
+
+    @GetMapping ("/items")
+    public List<Item> getAllItems(){ return itemService.findAll(); }
+
+    @GetMapping ("/items/{id}")
+    public Item getItem(@PathVariable String id) {
+        Item item = new Item();
+        item.setName("Julie");
+        return item;
     }
 }
